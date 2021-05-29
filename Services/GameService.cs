4875b5adc6a278a6d2474c1c07e1a57341c61a3c
Bruno Carvalhal <private>
2021-05-29 17:01:25 +0200

    Add square highlights and make GameService static

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -3,13 +3,26 @@ using ChessWasm.Models;
 
 namespace ChessWasm.Services
 {
-    public class GameService
+    public static class GameService
     {
-        public Game CurrentGame { get; private set; }
-        public Game StartGame() 
+        private static Random rand = new Random();
+        public static Game CurrentGame { get; private set; }
+
+        public static Game StartGame() 
         {
             CurrentGame = new Game();
             return CurrentGame;
+        }
+
+        public static void RandomMove()
+        {
+            int from = 0, to = 0;
+            while(from == to || CurrentGame.Board.Squares[from] == 0)
+            {
+                from = rand.Next(0, 64);
+                to = rand.Next(0, 64);
+            }
+            CurrentGame.MakeMove(from, to);
         }
     }
 }
